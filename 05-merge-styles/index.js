7524bdbf7fbcb32688b081424c217d246e120e7a
DenisPank <den3000d.1267@gmail.com>
2022-11-07 21:46:26 +0300

    fix: delite bag in six task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,27 +1,28 @@
-let fs = require('fs');
+let fs = require("fs");
 const fsPromises = fs.promises;
-const path = require('path');
+const path = require("path");
 
-const pathFiles = path.join(__dirname, 'styles');
-const pathToFiles = path.join(__dirname, 'project-dist');
+const pathFiles = path.join(__dirname, "../05-merge-styles", "styles");
+const pathToFiles = path.join(__dirname, "../05-merge-styles", "project-dist");
 
 // console.log(pathFiles, pathToFiles);
 
 async function mergeCss() {
   const arrayToFiles = await fsPromises.readdir(pathToFiles);
-  const pathToBundle = await path.join(pathToFiles, 'bundle.css');
-  if (!arrayToFiles.includes('bundle.css')) {
-    await fsPromises.writeFile(pathToBundle, '');
+  const pathToBundle = await path.join(pathToFiles, "bundle.css");
+  if (!arrayToFiles.includes("bundle.css")) {
+    await fsPromises.writeFile(pathToBundle, "");
   } else {
     await fsPromises.unlink(pathToBundle);
-    await fsPromises.writeFile(pathToBundle, '');
+    await fsPromises.writeFile(pathToBundle, "");
   }
   const arrayFiles = await fsPromises.readdir(pathFiles);
   for await (let item of arrayFiles) {
+    console.log(item);
     const arrayPathFiles = await path.join(pathFiles, item);
     const arrayExFiles = await path.extname(arrayPathFiles);
-    if (arrayExFiles === '.css') {
-      await fs.readFile(path.join(pathFiles, item), 'utf-8', (err, data) => {
+    if (arrayExFiles === ".css") {
+      await fs.readFile(path.join(pathFiles, item), "utf-8", (err, data) => {
         fsPromises.appendFile(pathToBundle, data);
       });
     }
--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,125 +1,14 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
+const { stdin, stdout } = process;
 
-// Прверка наличия файла "about.html"
-let hasAbout = false;
-
-fs.readdir(path.join(__dirname, 'components'), (err, data) => {
-    if (err) console.error(error.message);
-    if (data.includes('about.html')) {
-        hasAbout = true;
-    }
-})
-
-function getBundle() {
-    // Cоздание папки "project-dist"
-    fs.mkdir(path.join(__dirname, 'project-dist'), err => {
-        if (err) console.error(err.message);
-    }) 
-    
-    // Создание файла "index.html" внутри папки "project-dist"
- 
-    let template = '';
-        fs.readFile(path.join(__dirname, 'template.html'), 'utf-8', (err, data) => {
-            if (err) console.error(err.message);
-            template = data;
-            if (hasAbout === true) {
-                fs.readFile(path.join(__dirname, 'components', 'about.html'), 'utf-8', (err, data) => {
-                    if (err) console.error(err.message);
-                    const about = data;
-                    template = template.replace('{{about}}', about);
-                })
-            }
-            fs.readFile(path.join(__dirname, 'components', 'header.html'), 'utf-8', (err, data) => {
-                if (err) console.error(err.message);
-                const header = data;
-                template = template.replace('{{header}}', header);
-            });
-            fs.readFile(path.join(__dirname, 'components', 'articles.html'), 'utf-8', (err, data) => {
-                if (err) console.error(err.message);
-                const articles = data;
-                template = template.replace('{{articles}}', articles);
-            });
-            fs.readFile(path.join(__dirname, 'components', 'footer.html'), 'utf-8', (err, data) => {
-                if (err) console.error(err.message);
-                const footer = data;
-                template = template.replace('{{footer}}', footer);
-                fs.writeFile(path.join(__dirname, 'project-dist', 'index.html'), template, err => {
-                    if (err) console.error(err.message);
-                })
-            });
-        })
-    
-    // Создание файла "style.css" внутри папка "project-dist"
-    
-    fs.readdir(path.join(__dirname, 'styles'), (error, data) => {
-        if (error) console.error(error.message);
-        fs.writeFile(path.join(__dirname, 'project-dist', 'style.css'), '', (error) => {
-            if (error) console.error(error.message);
-        });
-        data.forEach(file => {
-            fs.readFile(path.join(__dirname, 'styles', file), 'utf-8', (error, data) => {
-                if (error) console.error(error.message);
-                fs.appendFile(path.join(__dirname, 'project-dist', 'style.css'), data, (error) => {
-                    if (error) console.error(error.message);
-                })
-            });
-        })
-    })
-    
-    // Копирование папки "assets" в папку "project-dist"
-    
-    fs.mkdir(path.join(__dirname, 'project-dist', 'assets'), (error) => {
-        if (error) console.error(error.message);
-    });
-    
-    fs.mkdir(path.join(__dirname, 'project-dist', 'assets', 'fonts'), err => {
-        if (err) console.error(err.message);
-        fs.readdir(path.join(__dirname, 'assets', 'fonts'), (error, data) => {
-            if (error) console.error(error.message);
-            data.forEach(file => {
-                fs.copyFile(path.join(__dirname, 'assets', 'fonts', file), path.join(__dirname, 'project-dist', 'assets','fonts', file), (error) => {
-                    if (error) console.error(error.message);
-                })
-            })
-        })
-    });
-    
-    fs.mkdir(path.join(__dirname, 'project-dist', 'assets', 'img'), err => {
-        if (err) console.error(err.message);
-        fs.readdir(path.join(__dirname, 'assets', 'img'), (error, data) => {
-            if (error) console.error(error.message);
-            data.forEach(file => {
-                fs.copyFile(path.join(__dirname, 'assets', 'img', file), path.join(__dirname, 'project-dist', 'assets','img', file), (error) => {
-                    if (error) console.error(error.message);
-                })
-            })
-        })
-    });
-    
-    fs.mkdir(path.join(__dirname, 'project-dist', 'assets', 'svg'), err => {
-        if (err) console.error(err.message);
-        fs.readdir(path.join(__dirname, 'assets', 'svg'), (error, data) => {
-            if (error) console.error(error.message);
-            data.forEach(file => {
-                fs.copyFile(path.join(__dirname, 'assets', 'svg', file), path.join(__dirname, 'project-dist', 'assets','svg', file), (error) => {
-                    if (error) console.error(error.message);
-                })
-            })
-        })
-    });
-}
-
-// Проверка наличия папки "project-dist"
-
-fs.readdir(path.join(__dirname), 'utf-8', (err, data) => {
-    if (err) console.error(err.message);
-    if (data.includes('project-dist')) {
-        fs.rm(path.join(__dirname, 'project-dist'), {recursive: true, force: true}, err => {
-            if (err) console.error(err.message);
-            getBundle();
-        });
-    } else {
-        getBundle();
-    }
-})
+let file;
+fs.readFile(
+  path.join(__dirname, "../06-build-page", "/template.html"),
+  "utf8",
+  (err, data) => {
+    if (err) throw err;
+    file = data.split("\n")[1];
+  }
+);
+console.log(file);
